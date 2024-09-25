953e554605b834bf16084c5f5121e7d94f1ad292
Hilary Fung <1105386+hilaryfung@users.noreply.github.com>
2024-09-25 11:51:40 -0700

    use elementbase from primaries rig

--- src/js/components/results-table/index.js
+++ src/js/components/results-table/index.js
@@ -1,1 +1,18 @@
-console.log("components/results-table/index.js");
+var ElementBase = require("../elementBase");
+require("./results-table.less");
+
+class ResultsTable extends ElementBase {
+  constructor() {
+    super();
+  }
+
+  static get template() {
+    return require("./_results-table.html");
+  }
+
+  connectedCallback() {
+    this.illuminate();
+  }
+}
+
+customElements.define("results-table", ResultsTable);
