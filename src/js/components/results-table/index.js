7759b92ae071814d9dfdb6908fe54ec424ca7689
Hilary Fung <1105386+hilaryfung@users.noreply.github.com>
2024-09-25 09:17:31 -0700

    add a very basic component

---
+++ src/js/components/results-table/index.js
@@ -0,0 +1,1 @@
+console.log("components/results-table/index.js");
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,18 @@
 // require("./lib/pym");
 
 require("./analytics");
+
+class ResultsTable extends HTMLElement {
+  constructor() {
+    super();
+
+    const shadow = this.attachShadow({ mode: 'open' });
+    const container = document.createElement('div');
+		const templateString = require("./components/results-table/_results-table.html");
+    container.innerHTML = templateString;
+
+    shadow.appendChild(container);
+  }
+}
+
+customElements.define("results-table", ResultsTable);
