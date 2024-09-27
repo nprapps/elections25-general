9898a7104245c896261c4514ab02788975381279
jadolphe <jadolphe@npr.org>
2024-09-27 11:54:56 -0400

    starting some election work, css style

--- src/js/components/balance-of-power/index.js
+++ src/js/components/balance-of-power/index.js
@@ -1,8 +1,14 @@
 var ElementBase = require("../elementBase");
+import InactiveSenateRaces from "inactive_senate_races.sheet.json";
 
 class BalanceOfPower extends ElementBase {
   constructor() {
     super();
+
+    this.isHouse = props.race == "house";
+    this.isSenate = props.race == "senate";
+    // Hardcoded # of seats needed for majority in Senate/house
+    this.seatsNeeded = this.isSenate ? 51 : 218;
   }
 
   static get template() {
