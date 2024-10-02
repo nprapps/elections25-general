573936fe3d77a7121d020d3ff1cfdd086723266a
Hilary Fung <1105386+hilaryfung@users.noreply.github.com>
2024-10-02 13:30:58 -0700

    render candidate bars using illuminate!

--- src/js/components/results-table/index.js
+++ src/js/components/results-table/index.js
@@ -1,6 +1,8 @@
 const ElementBase = require("../elementBase");
-// var dot = require("../../lib/dot");
-// var template = dot.compile(require("./_results-table.html"));
+const dot = require("../../lib/dot");
+const template = dot.compile(require("./_results-table.html"));
+const { classify, mapToElements, formatAPDate, formatTime } = require("../utils");
+
 
 class ResultsTable extends ElementBase {
   constructor() {
@@ -32,11 +34,31 @@ class ResultsTable extends ElementBase {
   render() {
     if (!this.data) return;
 
-    // let result = this.data.results[0];
-    // this.innerHTML = template({result});
+    let result = this.data.results[0];
 
     var elements = this.illuminate();
-    elements.updated.innerHTML = new Date(this.data.results[0].updated);
+
+    elements.updated.innerHTML = `
+      ${formatAPDate(new Date(result.updated))} at ${formatTime(new Date(result.updated))}
+    `;
+    let candidates = mapToElements(elements.tbody, result.candidates);
+
+    candidates.forEach(d => {
+      let data = d[0];
+      let el = d[1];
+      
+      el.classList.add("row");
+      el.classList.add(classify(data.party));
+
+      el.innerHTML = `
+        <span class="bar-container">
+          <span class="bar" style="width: ${data.percent * 100}%"></span>
+        </span>
+        <span class="name">${data.first} ${data.last}</span>
+        <span class="percentage">${data.percent * 100}%</span>
+        <span class="votes">${data.votes}</span>
+      `
+    });
   }
 }
 
--- src/js/components/utils.js
+++ src/js/components/utils.js
@@ -117,6 +117,15 @@ var toggleAttribute = function(element, attribute, force) {
   }
 };
 
+var classify = function(str) {
+  return (str + "").toLowerCase()
+    .replace(/\s+/g, '-')           // Replace spaces with -
+    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+    .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+    .replace(/^-+/, '')             // Trim - from start of text
+    .replace(/-+$/, '');            // Trim - from end of text
+};
+
 module.exports = {
   apMonths,
   inDays,
@@ -125,5 +134,6 @@ module.exports = {
   parseNPRDate,
   groupBy,
   mapToElements,
-  toggleAttribute
+  toggleAttribute,
+  classify
 }
