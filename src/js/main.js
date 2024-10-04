349f4ae81501da09e06667d69f23597576de1a78
jadolphe <jadolphe@npr.org>
2024-10-04 09:10:08 -0400

    adding in results table code

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 // require("./lib/pym");
 
 require("./analytics");
+require("./components/results-table");
 require("./components/balance-of-power-combined");
 require("./components/balance-of-power-bar");
+
+
