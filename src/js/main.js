8c479b806ea651efddfc27a0b2eb3fd3310ffdfe
jadolphe <jadolphe@npr.org>
2024-11-04 04:20:38 -0500

    remove individual calls to removeLoadingScreen and put it in main.js

--- src/js/main.js
+++ src/js/main.js
@@ -4,19 +4,16 @@ import { Sidechain } from '@nprapps/sidechain';
 import './nav.js';
 
 require("./analytics");
+require("./removeLoadingScreen");
 require("./components/results-table");
 require("./components/balance-of-power-combined");
 require("./components/balance-of-power-bar");
 require("./components/results-board");
-require("./components/results-board-display");
 require("./components/results-board-key");
 require("./components/board-president");
 require("./components/board-senate");
 require("./components/board-governor");
 require("./components/board-house");
-require("./components/nationalMap");
-require("./components/cartogram");
-require("./components/electoralBubbles");
 require("./components/county-map");
 require("./components/results-collection");
 
