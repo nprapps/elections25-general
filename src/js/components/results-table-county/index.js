048ed38b2446354d4b22c11f966bc1b44e26ed0e
Hilary Fung <1105386+hilaryfung@users.noreply.github.com>
2024-11-05 20:17:18 -0800

    county results table: only show tie if top two candidates have same vote count. closes #325. (#338)
    * use EEVP instead of % precincts reporting in county map tooltip and county table
    
    * on county results table, only show tie if top two candidates have same vote count

--- src/js/components/results-table-county/index.js
+++ src/js/components/results-table-county/index.js
@@ -186,6 +186,8 @@ class ResultsTableCounty extends ElementBase {
         var [a, b] = candidates;
         if (!a.votes) {
             return "-";
+        } else if (a.votes === b.votes) {
+            return "Tie";
         }
         var winnerMargin = a.percent - b.percent;
         return voteMargin({ party: getParty(a.party), margin: winnerMargin });
@@ -207,15 +209,13 @@ class ResultsTableCounty extends ElementBase {
     marginCell(candidates, leadingCand, topCands) {
         let voteMargin = "-";
         let party = getParty(candidates[0]?.party || "");
-        
+        if (candidates[0].votes === candidates[1].votes) {
+            party = "";
+        }
+
         if (topCands.includes(candidates[0].last)) {
             const calculatedMargin = this.calculateVoteMargin(candidates);
-            if (calculatedMargin.includes('+0')) {
-                voteMargin = 'Tie';
-                party = ''
-            } else {
-                voteMargin = calculatedMargin;
-            }
+            voteMargin = calculatedMargin;
         }
 
         return `<td class="vote margin ${party}">${voteMargin}</td>`;
@@ -251,7 +251,11 @@ class ResultsTableCounty extends ElementBase {
             metricValue = metric.format(metricValue);
         }
 
-        const leadingCand = row.eevp > 0 ? row.candidates[0] : "";
+        let leadingCand = row.eevp > 0 ? row.candidates[0] : "";
+        if (row.candidates[0].votes === row.candidates[1].votes) {
+            leadingCand = "";
+        }
+        
         const reportingPercent = reportingPercentage(row.eevp) + "% in";
         const candidateCells = candidates.map(c =>
             this.candidatePercentCell(
