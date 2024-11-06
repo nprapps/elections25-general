9c4119c3b22c65b68929c47c4175dc9c0af7a062
Shajia Abidi <abidishajia@gmail.com>
2024-11-05 17:55:53 -0800

    added the condition to remove special condition for forecast (#328)
    * added the condition to remove special condition for forecast
    
    * updated the variable
    
    ---------
    
    Co-authored-by: Shajia Abidi <sabidi@npr.org>

--- tasks/elex.js
+++ tasks/elex.js
@@ -128,7 +128,12 @@ module.exports = function (grunt) {
     await fs.mkdir("build/data/states", { recursive: true });
     const states = {};
     geo.state.forEach(function (result) {
-      if (result.office === "H" || result.office === "S") {
+      if (
+        (result.office === "H" &&
+          result.id !== "45888" &&
+          result.id !== "50068") ||
+        result.office === "S"
+      ) {
         dashboard.push({
           state: result.state,
           office: result.office,
